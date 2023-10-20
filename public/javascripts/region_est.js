dcf4fea6a0bd02eb48c29deaadaef3420153cf7e
Andrii Androsovych <kozakizdona@gmail.com>
2023-10-20 15:37:21 +0300

    45657

--- public/javascripts/region_est.js
+++ public/javascripts/region_est.js
@@ -10,12 +10,22 @@ window.onload = async function(){
   try{
     let resp = await fetch("../");
     let list = await resp.json();
-    let clue = new ClueInput(container,new Set(list),(arg)=>alert(arg)); 
+    let clue = new ClueInput(container,new Set(list),onNextStep); 
     clue.createFramework();
   }catch(e){
     alert(e);
   }
 
+function onNextStep(val){
+  let host = window.location.hostname;
+  //crfeate new URL
+  let url = new URL(`http://${host}/estate/new/districts/content/`);
+  //search params
+  url.searchParams.set("region",val);
+  //jump to the new URL
+  window.location.href=url.toString();
+
+}
    
  
 }
