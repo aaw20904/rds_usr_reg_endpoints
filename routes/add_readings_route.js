84b39ade2a481880af7530ab4c14542540be3661
Andrii Androsovych <kozakizdona@gmail.com>
2023-12-18 14:23:46 +0200

    123

---
+++ routes/add_readings_route.js
@@ -0,0 +1,11 @@
+const express= require("express");
+let b64ops = require("../base64json");
+let router = express.Router();
+let checker = require("./query_param_check");
+
+router.get("/start", async (req, res)=>{
+    
+})
+
+
+module.exports=router;
