9b048a6046c236691a062090747bbb7219a8b42d
Andrii Androsovych <kozakizdona@gmail.com>
2024-02-24 12:37:06 +0200

    hvmhgk

--- public/javascripts/add_provider/add_provider_step5.js
+++ public/javascripts/add_provider/add_provider_step5.js
@@ -19,14 +19,32 @@
  let btn = document.querySelector(".btn");
  let counter_id = document.querySelector("#counter_id"); 
  let provider_id= document.querySelector("#provider_id");
- let account = document.querySelector("#account");
 //assign values to form nodes
  counter_id.value = innerInfo.counter_id;
  provider_id.value = innerInfo.provider_id;
- account.value = innerInfo.account;
 //assign action link
  let form = document.querySelector("form");
  form.setAttribute("action",`http://${window.location.hostname}/providers/add/finish`);
+
+ //are credentials (for the provider) in th inner embedded object?
+ if(innerInfo["acc_num"]) {
+  //show elements
+   let elems = document.querySelectorAll(".provider_cred");
+   elems.forEach((s)=>{
+    s.classList.remove("d-none");
+   })
+   //assign values to each node (credentials):
+    document.getElementById("account").value = innerInfo.acc_num;
+     document.querySelector(".acc_string").innerText = innerInfo.acc_num;
+
+    document.getElementById('usr_login').value = innerInfo.usr_login;
+    document.querySelector('.login_string').innerText = innerInfo.usr_login;
+
+    document.getElementById('usr_password').value = innerInfo.usr_password;
+    document.querySelector('.password_string').innerText = innerInfo.usr_password;
+
+ }
+
 //deprecated
  btn.setAttribute("href",`http://${window.location.hostname}/providers/add/finish?provider_id=${innerInfo.provider_id}&counter_id=${innerInfo.counter_id}&account=${innerInfo.account}`)
 
